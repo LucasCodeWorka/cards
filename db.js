7ab921295e3d281ae00132a1f7a0567e0aaecb8d
Lucas Angelo <lucasangelo@MacBook-Air-de-Lucas.local>
2024-05-19 19:28:45 -0300

    feat: create api cards

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool1 = new Pool({
-    'user': 'erick.rodrigues',
-    'password': 'Hq2iNW4eCLjc',
-    'host': 'ep-rough-wave-13168438.us-east-2.aws.neon.tech',
-    'port': '5432',
-    'database': 'liebeti',
+  user: 'LucasCodeWorka',
+  password: 'IO24VirZxBgc',
+  host: 'ep-weathered-king-a59769hv.us-east-2.aws.neon.tech',
+  port: '5432',
+  database: 'liebe',
   ssl: {
     rejectUnauthorized: false,
     sslmode: 'require'
--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const { format } = require('date-fns');
 
 
 const pool = new Pool({
-    'user': 'erick.rodrigues',
-    'password': 'Hq2iNW4eCLjc',
-    'host': 'ep-rough-wave-13168438.us-east-2.aws.neon.tech',
-    'port': '5432',
-    'database': 'liebeti',
+  user: 'LucasCodeWorka',
+  password: 'IO24VirZxBgc',
+  host: 'ep-weathered-king-a59769hv.us-east-2.aws.neon.tech',
+  port: '5432',
+  database: 'liebe',
   ssl: {
     rejectUnauthorized: false,
     sslmode: 'require'
@@ -28,10 +28,10 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get('/todos', async (req, res) => {
+app.get('/vendas', async (req, res) => {
   try {
     const client = await pool1.connect();
-    const result = await client.query('SELECT * FROM projeto ');
+    const result = await client.query('select * from public.vendas_rep');
     const data = result.rows;
     client.release();
     res.json(data);
@@ -41,56 +41,56 @@ app.get('/todos', async (req, res) => {
   }
 });
 
-app.get("/rep", async (req, res) => {
-  try {
-      const allTodos = await pool1.query('SELECT * FROM projeto  ')
-      res.json(allTodos.rows)
-  } catch (err) {
-      console.error(err.message)
-  }
-})
+// app.get("/rep", async (req, res) => {
+//   try {
+//       const allTodos = await pool1.query('SELECT * FROM projeto  ')
+//       res.json(allTodos.rows)
+//   } catch (err) {
+//       console.error(err.message)
+//   }
+// })
 
-app.get("/todos/:id", async (req, res) => {
-  try {
-      const { id } = req.params;
-      const todo = await pool1.query("SELECT * FROM projeto WHERE id = $1  ", [id])
+// app.get("/todos/:id", async (req, res) => {
+//   try {
+//       const { id } = req.params;
+//       const todo = await pool1.query("SELECT * FROM projeto WHERE id = $1  ", [id])
   
-      res.json(todo.rows[0])
-  } catch (err) {
-      console.err(err.message);
-  }
-  })
+//       res.json(todo.rows[0])
+//   } catch (err) {
+//       console.err(err.message);
+//   }
+//   })
 
 
   //API do rep
 
-  app.get('/repri', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM public.vr_pes_reprcliente ');
-      const data = result.rows;
-      client.release();
-      res.json(data);
-    } catch (error) {
-      console.error('Erro ao buscar dados:', error);
-      res.status(500).json({ error: 'Erro interno do servidor' });
-    }
-  });
+  // app.get('/repri', async (req, res) => {
+  //   try {
+  //     const client = await pool.connect();
+  //     const result = await client.query('SELECT * FROM public.vr_pes_reprcliente ');
+  //     const data = result.rows;
+  //     client.release();
+  //     res.json(data);
+  //   } catch (error) {
+  //     console.error('Erro ao buscar dados:', error);
+  //     res.status(500).json({ error: 'Erro interno do servidor' });
+  //   }
+  // });
 
-  app.get("/repri/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const todo = await pool.query("SELECT * FROM public.vr_pes_reprcliente WHERE cd_cliente = $1  ", [id])
+  // app.get("/repri/:id", async (req, res) => {
+  //   try {
+  //       const { id } = req.params;
+  //       const todo = await pool.query("SELECT * FROM public.vr_pes_reprcliente WHERE cd_cliente = $1  ", [id])
     
-        res.json(todo.rows[0])
-    } catch (error) {
-        console.error(error.message);
-    }
-    })
+  //       res.json(todo.rows[0])
+  //   } catch (error) {
+  //       console.error(error.message);
+  //   }
+  //   })
   
   
 
 
-app.listen(port, () => {
-  console.log(`Servidor backend rodando na porta ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Servidor backend rodando na porta ${port}`);
+// });
